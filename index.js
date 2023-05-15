877c24ea4e9d3db2bfeb629561415394c401dacb
João Garrucho <joao.garrucho.work@gmail.com>
2023-05-15 17:03:49 +0100

    feat(person): add person

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 console.log("Hello World");
 
 const a = [1, 2, 10, 14, 5];
-a.forEach(n => console.log(n));
+a.forEach((n) => console.log(n));
 
 let maior = "joao";
+
+const person = {
+  name: maior,
+  age: 27,
+  sex: 5,
+};
